0d3285c03abba8f82e3c6b0c441d58439d52929e
Ahmed Tarek <ahmedtarek@baianat.com>
2022-06-02 22:43:32 +0200

    Initial Commit

---
+++ GoodsmartNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodsmartNews
+//
+//  Created by Ahmed Tarek on 02/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
